6266a0b6e998cf4eb315d1f707e9bfcc32602a61
Jules attivissimo <jules.attivissimo@gmail.com>
2018-03-19 15:05:37 +0100

    try send object via api

--- api/src/main.py
+++ api/src/main.py
@@ -4,14 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return {
-        'hello':'world',
-        'how': [
-            'are',
-            'you',
-            '?'
-        ]
-    }
+    return { 'hello':'world' }
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="0.0.0.0", threaded=True)
