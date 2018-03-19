214017c35404fe275feff7061490965853fdd6e2
Jules Attivissimo <jules.attivissimo@intech.lan>
2018-03-19 16:46:33 +0100

    try shell command

--- api/src/main.py
+++ api/src/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify, request
+import subprocess
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify({'hello': 'world'})
+    return str(subprocess.Popen("echo Hello World", shell=True, stdout=subprocess.PIPE).stdout.read())
 
 
 @app.route('/rich')
@@ -15,7 +16,6 @@ def rich():
         'message': 'This message if embeded in red.',
         'title': 'Red Alert'
     })
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="0.0.0.0", threaded=True)
