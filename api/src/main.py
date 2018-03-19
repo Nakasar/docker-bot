a18a34112893d630a8a5c34d55261223b4650147
Jules Attivissimo <jules.attivissimo@intech.lan>
2018-03-19 15:02:58 +0100

    try some api calls

--- api/src/main.py
+++ api/src/main.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return {
+        'hello':'world',
+        'how': [
+            'are',
+            'you',
+            '?'
+        ]
+    }
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="0.0.0.0", threaded=True)
