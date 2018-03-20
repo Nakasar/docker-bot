11598df136926b3cd0cbf43932f871f02d55d9ff
Kevin Thizy <kevin.thizy@intech.lu>
2018-03-20 13:16:45 +0100

    Improved list containers with container name and id.

--- api/src/dockerbot/infos/containers.py
+++ api/src/dockerbot/infos/containers.py
@@ -1,7 +1,26 @@
 def listContainers():
+    """
+    Gets list of running containers in current docker client.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    [Container]
+        List of Container objects
+    """
     global client
     try:
         containers = client.containers.list()
-        return containers
+        response = []
+        for container in containers:
+            response.append("{0} ({1})".format(container.name, container.id))
+        return response
     except:
         return []
+
+
+def detailContainer(containerId):
+    global client
+    return {}
