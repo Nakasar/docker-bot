105f7b516be1998764e734acdaeb3c1700b0bde3
jules.attivissimo <jules.attivissimo@gmail.com>
2018-03-21 11:41:33 +0100

    change --error effect

--- api/src/dockerbot/logs/containers.py
+++ api/src/dockerbot/logs/containers.py
@@ -33,18 +33,22 @@ def listLogs(container_name, limit = -1, error = False, since = '01-01/00:00:00'
         container = client.containers.get(container_name)
         data = {
             'title':'LOGS -- ' + container.name,
-            'message': "\n".join(str(container.logs(
-                stdout=True,
-                stderr=error,
-                since=datetime.strptime(since, '%m-%d/%H:%M:%S').replace(year=datetime.now().year),
-                until=datetime.strptime(until, '%m-%d/%H:%M:%S').replace(year=datetime.now().year)
-            ), 'utf-8').split('\n')[:limit])
+            'message': "\n".join(
+                [ element for element in (str(container.logs(
+                    stdout=True,
+                    stderr=True,
+                    since=datetime.strptime(since, '%m-%d/%H:%M:%S').replace(year=datetime.now().year),
+                    until=datetime.strptime(until, '%m-%d/%H:%M:%S').replace(year=datetime.now().year)
+                ), 'utf-8').split('\n')[:min(max(15, limit), 50)]) if ('error' in element.lower() and error or not error) ]
+            )
         } if since != None else {
             'title':'LOGS -- ' + container.name,
-            'message': "\n".join(str(container.logs(
-                stdout=True,
-                stderr=error
-            ), 'utf-8').split('\n')[:min(max(15, limit), 50)])
+            'message': "\n".join(
+                [ element for element in (str(container.logs(
+                    stdout=True,
+                    stderr=True
+                ), 'utf-8').split('\n')[:min(max(15, limit), 50)]) if ('error' in element.lower() and error or not error) ]
+            )
         }
         log("sending " + len(data.message) + " logs")
         return { "success":True, "data": data }
