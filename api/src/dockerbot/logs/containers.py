f3406321cb091d54be4df397c39b23d626bd1c42
jules.attivissimo <jules.attivissimo@gmail.com>
2018-03-21 11:46:43 +0100

    fix logs

--- api/src/dockerbot/logs/containers.py
+++ api/src/dockerbot/logs/containers.py
@@ -4,7 +4,7 @@ import sys
 
 client = docker.from_env()
 
-def log(*args, state='INFO', prefix='API'):
+def log(args, state='INFO', prefix='API'):
     print(prefix + ' :: ' + state + ' :: ' + args)
 
 def listLogs(container_name, limit = -1, error = False, since = '01-01/00:00:00', until = '12-31/00:00:00'):
@@ -32,23 +32,19 @@ def listLogs(container_name, limit = -1, error = False, since = '01-01/00:00:00'
         log("listing logs from " + container_name)
         container = client.containers.get(container_name)
         data = {
-            'title':'LOGS -- ' + container.name,
-            'message': "\n".join(
-                [ element for element in (str(container.logs(
-                    stdout=True,
-                    stderr=True,
-                    since=datetime.strptime(since, '%m-%d/%H:%M:%S').replace(year=datetime.now().year),
-                    until=datetime.strptime(until, '%m-%d/%H:%M:%S').replace(year=datetime.now().year)
-                ), 'utf-8').split('\n')[:min(max(15, limit), 50)]) if ('error' in element.lower() and error or not error) ]
-            )
+            'title':'LOGS : ' + container.name,
+            'message': "\n".join(str(container.logs(
+                stdout=True,
+                stderr=True,
+                since=datetime.strptime(since, '%m-%d/%H:%M:%S').replace(year=datetime.now().year),
+                until=datetime.strptime(until, '%m-%d/%H:%M:%S').replace(year=datetime.now().year)
+            ), 'utf-8').split('\n')[:limit])
         } if since != None else {
             'title':'LOGS -- ' + container.name,
-            'message': "\n".join(
-                [ element for element in (str(container.logs(
-                    stdout=True,
-                    stderr=True
-                ), 'utf-8').split('\n')[:min(max(15, limit), 50)]) if ('error' in element.lower() and error or not error) ]
-            )
+            'message': "\n".join(str(container.logs(
+                stdout=True,
+                stderr=True
+            ), 'utf-8').split('\n')[:min(max(15, limit), 50)])
         }
         log("sending " + len(data.message) + " logs")
         return { "success":True, "data": data }
